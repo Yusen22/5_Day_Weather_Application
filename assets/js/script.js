b6b11319d3838c928b453916d69f42f8e373eb82
Yusen22 <elijahwilsher@gmail.com>
2023-01-27 16:32:57 +0000

    added keyup functionality to autocomplete user search. Needs styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,24 +10,72 @@ var coordinates = {
 };
 
 
-// stores searched city name to a variable 
-var cityName = "london";
+
+var cityName
+
+var currentCities = []
+
 
 
 // stores the URL to call the 5 Day forecast API for seached city  
 var forecastQuery = "https://api.openweathermap.org/data/2.5/forecast?lat=" + coordinates.lat + "&lon=" + coordinates.long + "&appid=" + apiKey;
 
 
-// stores the query URL for converting a city name into coordinates 
-var geoQuery = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&limit=5&appid=" + apiKey;
+
+
+
+$("#search-input").keyup(function (event) {
+
+    event.preventDefault()
+    cityName = $(this).val()
+
+    // stores the query URL for converting a city name into coordinates 
+    var geoQuery = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&limit=5&appid=" + apiKey;
+    // AJAX call to convert city name to coordinates 
+    if (cityName.length >= 3) {
+        $.ajax({
+            url: geoQuery,
+            method: 'get'
+        }).then(function (response) {
+
+            // Saves current city object into a local array 
+            currentCities = response;
+            var availableTags = [];
+
+
+            // If there are more than one city with this name...
+
+            for (i in currentCities) {
+                var cityAndCode = currentCities[i].name + ", " + currentCities[i].country;
+                // Create an array with all cities and their country code 
+                availableTags.push(cityAndCode)
+
+                
+            }
+
+            $('#search-input').autocomplete({
+                source: availableTags
+            });
+
+            $('.ui-menu-item').css({"border": "1px solid black", "padding": "20px"})
+
+            coordinates.long = response.lon;
+            coordinates.lat = response.lat;
 
 
 
 
+        })
 
-$.ajax({
-    url: geoQuery,
-    method: 'get'
-}).then(function (response) {
-    console.log(response);
+    }
+
+
+});
+
+
+$('#search-button').on("click", function () {
+
+    // stores searched city name to a variable 
+    cityName = $('#search-input').val();
 })
+
